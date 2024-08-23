bddd0940c1596314cdf3d12b92d4df1f3df86044
Tannafama <143949496+tannafama@users.noreply.github.com>
2024-08-23 21:47:22 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,14 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    return data
+    data = generate_random_data()
+import random
+    for item in data:
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+
+
+    main()
+def generate_random_data():
